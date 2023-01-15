effab76a344227403772bbdba365912b13a2184b
AbdullaQasrawi <98874284+AbdullaQasrawi@users.noreply.github.com>
2023-01-15 13:15:28 +0200

    Add files via upload

---
+++ scr/Inference.py
@@ -0,0 +1,26 @@
+
+
+import pickle
+import pandas as pd
+with open('trained_model.pkl', 'rb') as f:
+    dt = pickle.load(f)
+
+
+def predict(model, new_data):
+    predictions = model.predict(new_data)
+    return predictions
+
+
+
+new_data = pd.read_csv("data/ACME-HappinessSurvey2020.csv")
+y = new_data['Y']
+new_data.drop('Y', axis=1, inplace=True)
+
+
+
+predictions = predict(dt, new_data)
+predictions
+
+
+
+
